b8699a13a13feda2f93362b9792e3f36081b5a75
Dana Chen <danaqchen@gmail.com>
2019-07-09 09:56:53 +0200

    adding more function and scoping exercises

--- practice_problems/var_scope.js
+++ practice_problems/var_scope.js
@@ -135,7 +135,7 @@ a is not declared anywhere either, so line 118 would produce a ReferenceError
 */
 
 // Lesson 1: Var scopes in JS (2)
-// * Problem 1
+// * Problem 1: scoping on a functional level or block level?
 function say() {
   if (false) {
     var a = 'hello from inside a block';
@@ -144,11 +144,21 @@ function say() {
 }
 say();
 
-
 /*
+On line 143, the console attempts to print out the value of variable `a`, this will return 'undefined'.
+We need to see the code after hoisting
 */
+function say() {
+  var a;
+  if (false) {
+    a = 'hello from inside a block';
+  }
+  console.log(a);
+}
+say();
+// In this case, `a` is declared but not assigned a value. Remember that JS is scoped at the functional level, not block-level.
 
-// * Problem 2
+// * Problem 2: global var and function scoping
 function hello() {
   a = 'hello';
   console.log(a);
@@ -158,19 +168,39 @@ function hello() {
 }
 hello();
 console.log(a);
+
+// After hoisting
+function hello() {
+  var a;
+  a = 'hello';
+  console.log(a);
+  if (false) {
+    a = 'hello again';
+  }
+}
+hello();
+console.log(a);
+
 /*
+On line 164, the console attempts to print out variable `a`, which has been initialized and assigned to hello.
+On line 169, the function `hello` is invoked
+On line 170, the console attempts to print out the value of `a` again, but `a` is not a global variable, its scope is the body of `hello()`
 */
 
-// * Problem 3
+// * Problem 3: Effect of hoisting var when global var already exists
 var a = 'hello';
 for (var i = 0; i < 5; i += 1) {
   var a = i;
 }
 console.log(a);
+
 /*
+On line 195, the console prints out the value of the global `a`. 
+The var declaration on line 193 is hoisted to the top of the global scope. Since a global variable `a` exists, the hoist has no direct effect on operations.
+Line 195 logs 4.
 */
 
-// * Problem 4
+// * Problem 4: return values from function, and multiple global var reassignments.
 var a = 1;
 function foo() {
   a = 2;
@@ -182,10 +212,14 @@ function foo() {
 }
 console.log(foo());
 console.log(a);
+
 /*
+Line 213 logs the function `foo()`, where it returns the value of the `bar` function, with a value of 4.
+Line 214 logs the var `a`, which is the global variable which value has been changed twice, first to 2, then to 3.
+Remember the var `a` reassigned within the function `foo()` is hoisted to the top, where they replace the value of the global var `a`.
 */
 
-// * Problem 5
+// * Problem 5: function return value vs. global/local var
 var a = 'global';
 function checkScope() {
   var a = 'local';
@@ -201,10 +235,13 @@ function checkScope() {
 }
 console.log(checkScope());
 console.log(a);
+
 /*
+On line 236, the console logs the return value of `checkScope()`, and we reach into the `superNested()` function with a return value of `superNested`
+On line 237, we look at the value of var `a`, each scope contains its own local var `a`, so the global `a` is never changed and stays at `global`
 */
 
-// * Problem 6
+// * Problem 6: var reassignment within a function
 var a = 'outer';
 var b = 'outer';
 console.log(a);
@@ -217,9 +254,12 @@ function setScope(foo) {
   b = 'inner';
 }
 /*
+Line 247 prints out `outer`, line 248 prints out `outer` as well.
+On line 250, `console.log(a)` prints out the same `outer` since assigning an argument to a function has no effect on it (assining a value to `foo` in this case).
+On line 251, the reassignment of `b` on line 254 takes effect, and `b` now holds the value `inner`.
 */
 
-// * Problem 7
+// * Problem 7: global var vs. function parameter of the same name
 var total = 50;
 var increment = 15;
 function incrementBy(increment) {
@@ -229,18 +269,35 @@ console.log(total);
 incrementBy(10);
 console.log(total);
 console.log(increment);
+
 /*
+Line 268 prints out 50, since the function parameter `increment` is not the same as the global var on 264
+Line 270 prints out 60, 10 is passed in as `increment`, and the global var total is updated
+Line 271 prints out 15, which is the global var
 */
 
-// * Problem 8
+// * Problem 8: function expression and var assignment of function
 var a = 'outer';
-
 console.log(a);
 setScope();
 console.log(a);
-
 var setScope = function () {
   a = 'inner';
 };
-/*
+
+// With hoisting
+var a;
+var setScope;
+a = 'outer';
+console.log(a);
+setScope();
+console.log(a);
+setScope = function () {
+  a = 'inner';
+};
+
+/* 
+Line 281 returns `outer`
+With hoisting, setScope is initialized but not assigned
+Since setScope() is assigned the function after the method invocation, line 293 would return setScope is not a function and exit
 */
