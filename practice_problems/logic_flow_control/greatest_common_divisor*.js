af211b924bcc0b1825a334a912213095fc7558d6
Dana Chen <danaqchen@gmail.com>
2019-07-11 02:27:09 +0200

    add common divisor prob

---
+++ practice_problems/logic_flow_control/greatest_common_divisor*.js
@@ -0,0 +1,58 @@
+/* 
+Create a function that computes the Greatest Common Divisor of two positive integers. 
+Need to find the number that can divide into both the num and denom, need to find the biggest. 
+Find the range of numbers from 2 to the denom, that can divide into both the denom and the nom
+Itereate from 2 ... denom
+if it can be divided into the num and the denom, it becomes the biggest divisor
+go to the next iteration, if that can also be divided into both numbers, it substitutes into the biggest divisor
+exit once it hits the denom
+*/
+
+function gcd(numerator, denom) {
+  biggestDivisor = 1;
+  for (i = 2; i <= denom; i++) {
+    if (numerator % i === 0 && denom % i === 0) {
+      biggestDivisor = i;
+    }
+  }
+  return biggestDivisor;
+}
+
+gcd(12, 4);
+
+// Further exploration: what if the numerator and denom is reversed?
+function gcd(num1, num2) {
+  numerator = Math.max(num1, num2);
+  denom = Math.min(num1, num2);
+  biggestDivisor = 1
+  for (i = 2; i <= denom; i++) {
+    if (numerator % i === 0 && denom % i === 0) {
+      biggestDivisor = i;
+    }
+  }
+  return biggestDivisor;
+}
+
+gcd(12, 4);
+
+/* Further exploration: accept an array of two or more numbers
+Hint:  To compute the GCD of 12, 4, and 8, for instance, you can compute:
+        gcd(12, gcd(4, 8));  // 4
+or,
+        gcd(gcd(12, 4), 8);  // 4
+*/
+
+function gcd(arr) {
+  numerator = arr[0];
+  gcd(arr) = arr[1..-1];
+  biggestDivisor = 1;
+  for (i = 2; i <= Math.min(denom); i++) {
+    if (numerator % i === 0 && denom % i === 0) {
+      biggestDivisor = i;
+    }
+  }
+  return biggestDivisor;
+}
+
+arr = (12, 4, 8);
+gcd(arr);
